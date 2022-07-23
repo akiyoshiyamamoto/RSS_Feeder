83623d9c900d3d91a98ff1b368d1071d206bc6f9
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2022-07-23 11:08:07 +0900

    Fix: ListRowViewの修正

--- RSS Feeder/View/ListRowView.swift
+++ RSS Feeder/View/ListRowView.swift
@@ -10,28 +10,33 @@ import SwiftUI
 struct ListRowView: View {
 	var item: RssItem
 	var body: some View {
-		VStack {
+		HStack {
+			VStack {
+				Text(item.title)
+					.font(.system(size: 14, design: .rounded))
+					.bold()
+					.frame(maxWidth: .infinity, minHeight: 90)
+					.multilineTextAlignment(.leading)
+				
+				Spacer()
+				
+				Text(item.pubDate)
+					.lineLimit(1)
+					.font(.system(size: 10, design: .rounded))
+					.frame(height: 10)
+			}
+			
 			AsyncImage(url: URL(string: item.thumbnail))
-				.frame(maxWidth: .infinity)
-				.cornerRadius(15)
+				.frame(width: 120, height: 100)
+				.cornerRadius(5)
 				.background(Color(.white))
-			
-			Text(item.title)
-				.font(.system(size: 20, design: .rounded))
-				.bold()
-				.frame(maxWidth: .infinity, alignment: .leading)
-				.padding(.top, 10)
-			
-			Text(item.description)
-				.font(.system(size: 16, design: .rounded))
-				.frame(maxWidth: .infinity, alignment: .leading)
-				.padding(.top, 5)
 		}
+		.frame(maxHeight: 100)
 	}
 }
 
 struct ListRowView_Previews: PreviewProvider {
 	static var previews: some View {
-		ListRowView(item: RssItem(title: "彫師でありながら現役藝大院生　アーティスト佐藤はなえが提案する「1mmタトゥー」", pubDate: "2020-10-12", link: "https://www.fashionsnap.com/article/hanaesato-interview/", guid: "https://www.fashionsnap.com/article/hanaesato-interview/", author: "", thumbnail: "https://res.fashionsnap.com/image/upload/w_300,h_300,c_fill,g_auto/asset/article/images/2022/06/1mmtatoo-interview_022.jpg", description: " アーティストの佐藤はなえは、東京藝術大学美術学部絵画科油絵専攻を修了後、同大学院で美術解剖学を学ぶ24歳だ。彫師でありながらも現役の藝大院生。"))
+		ListRowView(item: RssItem(title: "彫師でありながら現役藝大院生　アーティスト佐藤はなえが提案する「1mmタトゥー」", pubDate: "2020-10-12", link: "https://www.fashionsnap.com/article/hanaesato-interview/", guid: "https://www.fashionsnap.com/article/hanaesato-interview/", author: "田中康太", thumbnail: "https://res.fashionsnap.com/image/upload/w_300,h_300,c_fill,g_auto/asset/article/images/2022/06/1mmtatoo-interview_022.jpg", description: " アーティストの佐藤はなえは、東京藝術大学美術学部絵画科油絵専攻を修了後、同大学院で美術解剖学を学ぶ24歳だ。彫師でありながらも現役の藝大院生。"))
 	}
 }
